bb33b711f8a59512fa4c71bffec41fa23d064441
Giselly Rocha <104739434+Gisellyrock@users.noreply.github.com>
2023-10-05 09:09:48 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$nomeCliente = 'Leonardo';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP com HTML</title>
+</head>
+<body>
+    <h1>Olá <?php echo $nomeCliente; ?></h1>
+    <h1> Pagina do php</h1>
+</body>
+</html>
---
+++ variaveis.php
@@ -0,0 +1,9 @@
+<?php
+
+$variavel1 = "texto";
+echo $variavel1;
+
+$negativa = -288;
+$calculo = 200 + 555;
+
+echo $negativa + 129;
